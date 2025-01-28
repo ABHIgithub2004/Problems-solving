168b906ee25ab2d51c1bd724616f4e84577c7555
Abhi.S <151519373+ABHIgithub2004@users.noreply.github.com>
2025-01-28 19:51:13 +0530

    Substring.java

--- substring.java
+++ substring.java
@@ -2,9 +2,10 @@ public class substring{
     public static void main(String[] args){
         String s = "sak";
 
+        // Nested for loops are used to iterate and get the range to print
         for(int i = 0; i < s.length(); i++){
             for(int j = i+1; j <= s.length(); j++){
-                System.out.println(s.substring(i,j));
+                System.out.println(s.substring(i,j)); // This method prints the elements from the range of i to j
             }
         }
     }
