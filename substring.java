a251e6ac0c86b99289d842e07e172caa5f4fa789
Abhi.S <151519373+ABHIgithub2004@users.noreply.github.com>
2025-01-28 19:48:42 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,11 @@
+public class substring{
+    public static void main(String[] args){
+        String s = "sak";
+
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i+1; j <= s.length(); j++){
+                System.out.println(s.substring(i,j));
+            }
+        }
+    }
+}
