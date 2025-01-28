15f54c4c68af15f4f7db4c9b77a9c7ea9f2e9512
Abhi.S <151519373+ABHIgithub2004@users.noreply.github.com>
2025-01-28 19:47:21 +0530

    ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,13 +1,15 @@
 public class ReverseString{
     public static void main(String[] args){
         String s1 = "abcd";
-
+       
+        // Empty string to store the reverse string
         String res = "";
-
+       
+        //iterating from last index of the string
         for(int i = s1.length()-1; i >= 0; i--){
-            res += s1.charAt(i);
-            
+            res += s1.charAt(i); // Add  each character from the string at each iteration
         }
+        //Printing the reversed string
         System.out.println(res);
     }
 }
