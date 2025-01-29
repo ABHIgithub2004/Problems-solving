f8c5becf074e88ccb23c052a6ab54ad8f597a5df
Abhi.S <151519373+ABHIgithub2004@users.noreply.github.com>
2025-01-29 20:55:22 +0530

    AdditionSubtraction.java

--- AdditionSubtraction.java
+++ AdditionSubtraction.java
@@ -8,13 +8,18 @@ public class AdditionSubtraction{
         int n1 = in.nextInt();
         int n2 = in.nextInt();
 
+        // Adding two nummbers
         int sum = n1 + n2;
+
+        //Subtracting two numbers
         int sub = n1 - n2;
 
+        // if any negative value in sum of sub , convert them to positive using Absolute method
         if(sum < 0 || sub < 0){
            sum =  Math.abs(sum);
            sub =  Math.abs(sub);
         }
+        //printing them
         System.out.println("Addition of two numbers : " + sum);
         System.out.println("Subtraction of two numbers : " + sub);
 
