6ba12e3ae8b99e5516b90722c16ec162f1c163a9
Abhi.S <151519373+ABHIgithub2004@users.noreply.github.com>
2025-01-30 20:37:55 +0530

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,9 @@
+public class Forloop{
+    public static void main(String[] args){
+
+        //Loop breaks at 20
+        for(int i = 0; i < 20; i++){
+            System.out.println(i+1 + " ALL IS WELL !!!!"); // prints 20 times
+        }
+    }
+}
